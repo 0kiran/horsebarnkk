79d56ff3cbd89033a2425e489ecfc0de39b57a1f
BT_1N3_23@BKTECH.LOCAL <BT_1N3_23@BKTECH.LOCAL>
2018-11-15 14:47:28 -0500

    Initial commit

---
+++ src/HorseBarn.java
@@ -0,0 +1,26 @@
+public class HorseBarn {
+    private Horse[] spaces;
+    
+
+    public int findHorseSpace(String name){
+        for(int i = 0; i < this.spaces.length; i++){
+            if (this.spaces[i]!=null && name.equals(this.spaces[i].getName())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void consolidate(){
+        Horse[] newSpaces = new Horse[this.spaces.length];
+        int nextSpot = 0;
+        for (Horse nextHorse : this.spaces) {
+            if (nextHorse != null) {
+                newSpaces[nextSpot] = nextHorse;
+                nextSpot++;
+            }
+        }
+        this.spaces = newSpaces;
+    }
+
+}
